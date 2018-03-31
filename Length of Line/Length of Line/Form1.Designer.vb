05d8fd8617016ec62f4a607befacd1b6287cc70f
kab123 <kabdulyalah@gmail.com>
2018-03-30 22:02:14 -0700

    Add files via upload

---
+++ Length of Line/Length of Line/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.txtEndPoints = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(57, 82)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(301, 276)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(42, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(340, 31)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Length of Line Calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(54, 409)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "X1,Y1,X2,Y2"
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(174, 425)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(75, 23)
+        Me.btnCompute.TabIndex = 3
+        Me.btnCompute.Text = "Compute"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'txtEndPoints
+        '
+        Me.txtEndPoints.Location = New System.Drawing.Point(48, 425)
+        Me.txtEndPoints.Name = "txtEndPoints"
+        Me.txtEndPoints.Size = New System.Drawing.Size(100, 20)
+        Me.txtEndPoints.TabIndex = 4
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.Color.White
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(282, 427)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(76, 21)
+        Me.lblResult.TabIndex = 5
+        Me.lblResult.Text = "C="
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 509)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtEndPoints)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents txtEndPoints As TextBox
+    Friend WithEvents lblResult As Label
+End Class
---
+++ Length of Line/Length of Line/Form1.vb
@@ -0,0 +1,39 @@
+﻿
+
+Imports System.Math
+
+Public Class Form1
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim x1 As Double
+        Dim y1 As Double
+        Dim x2 As Double
+        Dim y2 As Double
+        Dim c As Double
+        Try
+            Dim endpoints() As String = Split(txtEndPoints.Text, ",", 4)
+            x1 = CDbl(endpoints(0))
+            y1 = CDbl(endpoints(1))
+            x2 = CDbl(endpoints(2))
+            y2 = CDbl(endpoints(3))
+
+            c = ComputeLineLength(x1, y1, x2, y2)
+            lblResult.Text = "C = " & c.ToString("N2")
+        Catch ex As Exception
+            MsgBox("Illegal input")
+        End Try
+    End Sub
+    Private Function ComputeLineLength(ByVal x1 As Double, ByVal y1 As Double,
+                                       ByVal x2 As Double, ByVal y2 As Double)
+        Dim a As Double
+        Dim b As Double
+        Dim hypotenous As Double
+        a = x1 - x2
+        b = y1 - y2
+        hypotenous = Sqrt(a ^ 2 + b ^ 2)
+        Return hypotenous
+    End Function
+
+    Private Sub txtEndPoints_Enter(sender As Object, e As EventArgs) Handles txtEndPoints.Enter
+        txtEndPoints.Text = String.Empty
+    End Sub
+End Class
