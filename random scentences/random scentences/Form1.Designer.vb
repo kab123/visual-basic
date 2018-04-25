798d182df83ead4d8bb9e0be2061ffe98570f1b9
kab123 <kabdulyalah@gmail.com>
2018-04-25 11:51:23 -0700

    Add files via upload

---
+++ random scentences/random scentences/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnRandom = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(31, 81)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(513, 620)
+        Me.lblOutput.TabIndex = 0
+        '
+        'btnRandom
+        '
+        Me.btnRandom.Location = New System.Drawing.Point(604, 162)
+        Me.btnRandom.Name = "btnRandom"
+        Me.btnRandom.Size = New System.Drawing.Size(225, 95)
+        Me.btnRandom.TabIndex = 1
+        Me.btnRandom.Text = "Randomize!"
+        Me.btnRandom.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(604, 313)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(225, 95)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(911, 852)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnRandom)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Name = "Form1"
+        Me.Text = "Random Sentences v1."
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnRandom As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ random scentences/random scentences/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub btnRandom_Click(sender As Object, e As EventArgs) Handles btnRandom.Click
+        Dim article() As String = {"A", "some", "the", "one", "any"}
+        Dim adjictive() As String = {"fast", "quick", "slow", "red", "blue", "green"}
+        Dim noun() As String = {"house", "plane", "dog", "camel", "sea horse"}
+        Dim verb() As String = {"runs", "screams", "eats", "codes", "crawls", "walked"}
+        Dim preposition() As String = {"to", "over", "under", "around"}
+
+        Dim index As Integer
+        Dim i As Integer
+        Dim sentence As String
+        Dim rand As New Random
+
+        lblOutput.Text = String.Empty
+        For i = 1 To 20
+            index = rand.Next(article.Length)
+            sentence = article(index)
+            Mid(sentence, 1, 1) = UCase(Mid(sentence, 1, 1))
+            sentence &= " "
+            sentence &= adjictive(index)
+            sentence &= " "
+            sentence &= noun(rand.Next(noun.Length))
+            sentence &= " "
+            sentence &= verb(index)
+            sentence &= " "
+            sentence &= preposition(rand.Next(preposition.Length))
+            sentence &= " "
+            sentence &= article(index)
+            sentence &= " "
+            sentence &= noun(rand.Next(noun.Length)) & "."
+            sentence &= vbCrLf
+            lblOutput.Text &= sentence
+        Next i
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
