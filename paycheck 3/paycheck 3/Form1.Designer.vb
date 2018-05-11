ead2bb4ba5cc7c37f37e68e47ea7764a687735ac
kab123 <kabdulyalah@gmail.com>
2018-05-11 12:54:31 -0700

    Add files via upload

---
+++ paycheck 3/paycheck 3/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.btnPrintPreview = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lstPayCheck = New System.Windows.Forms.ListBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.txtRate = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(185, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(311, 55)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "GET PAID!!!!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(103, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(103, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Hours"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(103, 269)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Rate"
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(579, 349)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(170, 59)
+        Me.btnCompute.TabIndex = 4
+        Me.btnCompute.Text = "Compute Pay"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'btnPrintPreview
+        '
+        Me.btnPrintPreview.Location = New System.Drawing.Point(579, 426)
+        Me.btnPrintPreview.Name = "btnPrintPreview"
+        Me.btnPrintPreview.Size = New System.Drawing.Size(170, 59)
+        Me.btnPrintPreview.TabIndex = 5
+        Me.btnPrintPreview.Text = "Print Preview"
+        Me.btnPrintPreview.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(579, 504)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(170, 59)
+        Me.btnPrint.TabIndex = 6
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(579, 582)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(170, 59)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(579, 657)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(170, 59)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lstPayCheck
+        '
+        Me.lstPayCheck.FormattingEnabled = True
+        Me.lstPayCheck.ItemHeight = 25
+        Me.lstPayCheck.Location = New System.Drawing.Point(31, 387)
+        Me.lstPayCheck.Name = "lstPayCheck"
+        Me.lstPayCheck.Size = New System.Drawing.Size(499, 329)
+        Me.lstPayCheck.TabIndex = 9
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(212, 150)
+        Me.txtName.Multiline = True
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(256, 40)
+        Me.txtName.TabIndex = 10
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(212, 208)
+        Me.txtHours.Multiline = True
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(112, 40)
+        Me.txtHours.TabIndex = 11
+        '
+        'txtRate
+        '
+        Me.txtRate.Location = New System.Drawing.Point(212, 266)
+        Me.txtRate.Multiline = True
+        Me.txtRate.Name = "txtRate"
+        Me.txtRate.Size = New System.Drawing.Size(112, 40)
+        Me.txtRate.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(791, 844)
+        Me.Controls.Add(Me.txtRate)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lstPayCheck)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnPrintPreview)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "paycheck 3.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents btnPrintPreview As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lstPayCheck As ListBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtHours As TextBox
+    Friend WithEvents txtRate As TextBox
+End Class
---
+++ paycheck 3/paycheck 3/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    'since I am unemployed these are sample income taxes
+    Const FedTaxRate As Decimal = 0.15
+    Const StateTaxRate As Decimal = 0.05
+    Const SDItaxRate As Decimal = 0.01
+    Const FICAtaxRate As Decimal = 0.062
+
+    'global variables
+    Dim employee As String
+    Dim hours As Decimal
+    Dim rate As Decimal
+    Dim regHours As Decimal
+    Dim otHours As Decimal
+    Dim regPay As Decimal
+    Dim otPay As Decimal
+    Dim grossPay As Decimal
+    Dim fedTax As Decimal
+    Dim stateTax As Decimal
+    Dim SDI As Decimal
+    Dim FICA As Decimal
+    Dim deductions As Decimal
+    Dim netPAy As Decimal
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ClearForm()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearForm()
+    End Sub
+
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        ComputePAy()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnPrintPreview_Click(sender As Object, e As EventArgs) Handles btnPrintPreview.Click
+
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+
+    End Sub
+
+    Private Sub ClearForm()
+        txtName.Text = String.Empty
+        txtHours.Text = String.Empty
+        txtRate.Text = String.Empty
+        lstPayCheck.Items.Clear()
+    End Sub
+
+    Private Sub ComputePAy()
+        Try
+            employee = txtName.Text
+            hours = CDec(txtHours.Text)
+            rate = CDec(txtRate.Text)
+            If hours < 0 Or hours > 168 Then
+                Throw New Exception("Illegal entry. Try again")
+            End If
+            If hours <= 40 Then
+                regHours = hours
+                otHours = 0
+            Else
+                regHours = 40
+                otHours = hours - 40
+            End If
+        Catch
+            MessageBox.Show("Illegal entry. Try again")
+        End Try
+    End Sub
+End Class
